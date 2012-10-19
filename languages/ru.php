bea65cd2e11e6f0cc12157cf84fb6ce6c894a543
cmb69 <cmbecker69@gmx.de>
2012-10-19 15:37:54 +0000

    added Slovak and Russian translations (contributed by Tata resp. Old)

---
+++ languages/ru.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * Copyright (c) 2012 Old aka Lybomyr Kydray
+ *
+ * Translation are licensed under the GNU General Public License, version 2 or
+ * later.
+ */
+
+$plugin_tx['handheld']['cf_mode']="Режим для мобильных браузеров: \"\" (отсутствует), \"1\" (перенаправление на другой домен), \"2\" (перенаправление на субсайт CMSimple) или \"3\" (использовать другой шаблон).";
+$plugin_tx['handheld']['cf_destination']="Полный абсолютный действующий URL для перенаправления мобильных браузеров, например, \"http://m.example.com/\". Необходим режим <b>1</b>.";
+$plugin_tx['handheld']['cf_subsite']="Имя субсайта CMSimple для перенаправления мобильных браузеров, например, \"mobile\". Необходим режим <b>2</b>.";
+$plugin_tx['handheld']['cf_template']="Имя шаблона, который следует использовать для мобильных браузеров. Необходим режим <b>3</b>.";
+$plugin_tx['handheld']['error_redirect_caption']="<b>Перенаправление невозможно!</b>";
+$plugin_tx['handheld']['error_redirect_details']="Заголовки уже отправлены: {file}, строка {line}.";
+$plugin_tx['handheld']['syscheck_title']="Проверка системы";
+$plugin_tx['handheld']['syscheck_phpversion']="Версия PHP &ge; %s";
+$plugin_tx['handheld']['syscheck_extension']="Расширение '%s' загружено";
+$plugin_tx['handheld']['syscheck_encoding']="Кодировка 'UTF-8' установлена";
+$plugin_tx['handheld']['syscheck_magic_quotes']="Экранирование спецсимволов (\"magic_quotes_runtime\") отключено";
+$plugin_tx['handheld']['syscheck_writable']="Папка '%s' доступна для записи";
+
+?>
---
+++ languages/sk.php
@@ -0,0 +1,16 @@
+<?php
+
+$plugin_tx['handheld']['cf_mode']="Mód pre mobilné prehliadače: \"\" (nevykoná nič), \"1\" (presmeruje na inú doménu), \"2\" (presmeruje na podstránku CMSimple) alebo \"3\" (použije iný template)";
+$plugin_tx['handheld']['cf_destination']="Presná a úplná URL,  na ktorú má byť premserovaný mobilný prehliadač, napr. \"http://m.example.com/\". (vyžaduje mód=1)";
+$plugin_tx['handheld']['cf_subsite']="Názov podstránky CMSimple, na ktorú budú presmerované mobilné prehliadače, napr. \"mobile\". (vyžaduje mód=2)";
+$plugin_tx['handheld']['cf_template']="Názov templatu, ktorý sa použije pre mobilné prehliadače (vyžaduje mód=3)";
+$plugin_tx['handheld']['error_redirect_caption']="<b>Presmerovanie nie je možné!</b>";
+$plugin_tx['handheld']['error_redirect_details']="Hlavička už bola odoslaná z {file} roadok {line}.";
+$plugin_tx['handheld']['syscheck_title']="Kontrola systému";
+$plugin_tx['handheld']['syscheck_phpversion']="PHP verzia &ge; %s";
+$plugin_tx['handheld']['syscheck_extension']="Rozšírenie '%s' načítané";
+$plugin_tx['handheld']['syscheck_encoding']="Kódovanie 'UTF-8' nastaven=";
+$plugin_tx['handheld']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['handheld']['syscheck_writable']="Adresár '%s' umožňuje zápis";
+
+?>
