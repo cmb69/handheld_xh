2d1eb4dfb425c997258634bd731d80f9ca6a65e0
cmb69 <cmbecker69@gmx.de>
2012-10-19 13:07:23 +0000

    added German translation

---
+++ languages/de.php
@@ -0,0 +1,18 @@
+<?php
+
+$plugin_tx['handheld']['cf_mode']="Der Modus für mobile Browser: \"\" (keine Sonderbehandlung), \"1\" (auf andere Domain weiter leiten), \"2\" (auf eine CMSimple Subsite weiter leiten) or \"3\" (ein anderes Template verwenden)";
+$plugin_tx['handheld']['cf_destination']="Die vollständig qualifizierte URL auf die mobile Browser weiter geleitet werden sollen, z.B. \"http://m.example.com/\". (falls mode=1)";
+$plugin_tx['handheld']['cf_subsite']="Der Name der CMSimple Subsite auf die mobile Browser weiter geleitet werden sollen, z.B. \"mobil\". (falls mode=2)";
+$plugin_tx['handheld']['cf_template']="Der Name des Templates, das für mobile Browser verwendet werden soll. (falls mode=3)";
+
+$plugin_tx['handheld']['error_redirect_caption']="<b>Weiterleitung nicht möglich!</b>";
+$plugin_tx['handheld']['error_redirect_details']="Die HTTP-Header wurden bereits in Datei {file} in Zeile {line} gesendet.";
+
+$plugin_tx['handheld']['syscheck_title']="System-Prüfung";
+$plugin_tx['handheld']['syscheck_phpversion']="PHP-Version ≥ %s";
+$plugin_tx['handheld']['syscheck_extension']="Erweiterung '%s' geladen";
+$plugin_tx['handheld']['syscheck_encoding']="UTF-8-Kodierung eingestellt";
+$plugin_tx['handheld']['syscheck_magic_quotes']="magic_quotes_runtime off";
+$plugin_tx['handheld']['syscheck_writable']="Ordner '%s' beschreibbar";
+
+?>
