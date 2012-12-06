3d457c3471fafe943b9ad84a2f5c91e95532c58e
cmb69 <cmbecker69@gmx.de>
2012-12-06 22:36:29 +0000

    added Czech localization and CTIMNE (contributed by oldnema)

---
+++ languages/cs.php
@@ -0,0 +1,16 @@
+<?php
+
+$plugin_tx['handheld']['cf_mode']="Mód pro mobilní prohlížeče: \"\" (nevykoná nic), \"1\" (přesměruje na jinou doménu), \"2\" (přesměruje na podstránku CMSimple) nebo \"3\" (použije jiný template)";
+$plugin_tx['handheld']['cf_destination']="Přesná a úplná URL,  na kterou má být přesměrovaný mobilní prohlížeč, např. \"http://m.example.com/\". (vyžaduje mód=1)";
+$plugin_tx['handheld']['cf_subsite']="Název podstránky CMSimple, na kterou budou přesměrovány mobilní prohlížeče, např. \"mobile\". (vyžaduje mód=2)";
+$plugin_tx['handheld']['cf_template']="Název templatu, který se použije pro mobilní prehlížeče (vyžaduje mód=3)";
+$plugin_tx['handheld']['error_redirect_caption']="<b>Přesměrování nelze uskutečnit!</b>";
+$plugin_tx['handheld']['error_redirect_details']="Hlavička už byla odeslána z {file} řádek {line}.";
+$plugin_tx['handheld']['syscheck_title']="Kontrola systému";
+$plugin_tx['handheld']['syscheck_phpversion']="PHP verze &ge; %s";
+$plugin_tx['handheld']['syscheck_extension']="Rozšíření '%s' načteno";
+$plugin_tx['handheld']['syscheck_encoding']="Kódování 'UTF-8' nastaveno";
+$plugin_tx['handheld']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['handheld']['syscheck_writable']="Adresář '%s' je zapisovatelný";
+
+?>
